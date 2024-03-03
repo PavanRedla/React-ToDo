41aeb04b5389fcb4bd871df01e15e8bab7d4e8dd
Pavan Kalyan Redla <pavankalyanredla42@gmail.com>
2024-03-03 22:11:47 +0530

    new

--- src/components/AddToDo.js
+++ src/components/AddToDo.js
@@ -1,7 +1,20 @@
-import React from 'react'
-
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../actions/actions";
 export default function AddToDo() {
+  const dispatch = useDispatch();
+  const myRef1 = useRef();
+
+  const addNewToDo = () => {
+    const todoText = myRef1.current.value;
+    const actionObj = addTodo(todoText);
+    dispatch(actionObj);
+  };
   return (
-    <div>AddToDo</div>
-  )
+    <>
+      <h1>Add a new todo</h1>
+      <input ref={myRef1} /> &nbsp;
+      <button onClick={addNewToDo}>Add New ToDO</button>
+    </>
+  );
 }
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,18 +1,43 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { deleteTodo } from "../actions/actions";
 
 // to read the data from redux and display it in component we use "useSelector"
 export default function ToDoList() {
+  const dispatch = useDispatch();
+
   const allTodos = useSelector((state) => {
     console.log(state.todosReducer);
     return state.todosReducer; // this indicates that data coming from todosReducer will be collected by allTodods.
   });
 
+  const deleteMyTodo = (id) => {
+    const actionObj = deleteTodo(id);
+    dispatch(actionObj);
+    // dispacth is used to connect with reducer
+  };
+
   return (
     <>
       <h3>ToDoList</h3>
       <table className="table table-bordered table-striped">
-        
+        {allTodos.map((todo) => {
+          return (
+            <tr key={todo.id}>
+              <td>{todo.id}</td>
+              <td>{todo.text}</td>
+              <td>{todo.completed.toString()}</td>
+              <td>
+                <button
+                  className="btn btn-danger mb-2"
+                  onClick={() => deleteMyTodo(todo.id)}
+                >
+                  DELETE
+                </button>
+              </td>
+            </tr>
+          );
+        })}
       </table>
     </>
   );
